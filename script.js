4ad28bece0608d7f2babf62428c3071460d6d3a4
Osagie Bello <info@techstackspace.com>
2026-05-01 13:22:45 +0100

    feat: add first JavaScript program with variables, objects, and console output

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello World!");
+
+const personName = "Adams";
+console.log(personName);
+console.log(`My name is ${personName}. I love JavaScript.`);
+
+console.log(3 + 4);
+
+// const personAge = prompt("What is your age?");
+// alert(`My age is ${personAge}.`);
+
+const objectCar = {
+    tyre: 4,
+    color: "red",
+    name: "BMW",
+    driver: {
+        name: "John Doe"
+    }
+}
+
+console.log(objectCar.tyre);
+console.log(objectCar.driver.name);
