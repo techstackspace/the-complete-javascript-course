e5d0df1ffa7d622c8bdf3788b1628b333d498429
Osagie Bello <info@techstackspace.com>
2026-05-18 17:53:31 +0100

    feat: add JavaScript data types examples and typeof demonstrations

--- script.js
+++ script.js
@@ -1,16 +1,91 @@
-const personName = "John Doe";
-console.log(personName);
+let favoriteNumber = 34; // number
+favoriteNumber = "69"; // string
+console.log(favoriteNumber);
 
-let personAge = 45;
-personAge = 54;
+// Data Types
+// 1. Number
+//    - Integer
+//    - Floating point
+//    - Infinity
+//    - -Infinity
+//    -  NaN
+
+const userWeight = 34.6;
+console.log(userWeight, Infinity);
+console.log(2 / 0);
+console.log("This is a text" / 34);
+console.log(NaN ** 0);
+
+// BigInt
+console.log(2 ** 53 - 1);
+// 9007199254740991
+
+const maxNum = 2 ** 53 - 1;
+console.log(maxNum + 1);
+console.log(maxNum + 2);
+
+const minNum = -(2 ** 53 - 1);
+console.log(minNum - 1);
+console.log(minNum - 2);
+
+const sandCount = 1212232342352535235353523534n;
+console.log(sandCount);
+
+// String
+const currentUser = "John Doe";
+const pageLoadTime = "23ms";
+const personAddress = `dsadaevwvwv`;
+
+console.log(currentUser, pageLoadTime, personAddress);
+
+// Boolean
+// True or False
+console.log(true, false);
+const isOlder = 34 > 23;
+console.log(isOlder);
+
+const isYounger = 34 < 23;
+console.log(isYounger);
+
+// Null
+let age = null;
+age = 45;
+console.log(age);
+
+// undefined
+let personAge;
 console.log(personAge);
 
-// const myCurrentAge = 67
-const MY_BIRTHDAY = "1993-11-03";
-const myAge = 34;
-const PI = 3.143;
+// Symbol
+Symbol("id");
 
-console.log(MY_BIRTHDAY, myAge, PI);
+// Objects (Non-Primitive)
+const myTechStack = ["Bun", "Node", "JavaScript"];
+myTechStack[1] = "Demo";
+console.log(myTechStack);
 
-console.log("Hello World!!");
-// [1, 4, 6].forEach(alert);
+const personInfo = {
+	userName: "Michael",
+	age: 78,
+	hasKid: true,
+};
+
+personInfo.userName = "Osagie";
+console.log(personInfo);
+
+// Typeof Operator
+
+console.log(typeof favoriteNumber);
+console.log(typeof userWeight);
+console.log(typeof sandCount);
+console.log(typeof currentUser);
+console.log(typeof isOlder);
+console.log(typeof age);
+console.log(typeof personAge);
+console.log(typeof myTechStack);
+console.log(typeof personInfo);
+console.log(typeof undefined);
+console.log(typeof {});
+console.log(typeof []);
+// console.log(typeof null);
+console.log(typeof (3 + 6));
