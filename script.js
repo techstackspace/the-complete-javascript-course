10f4e4cc2e75acbb42f13f2cf7a9e8b4d1f2f1e3
Osagie Bello <info@techstackspace.com>
2026-05-31 13:53:52 +0100

    feat(teaching): add string comparison and type coercion examples

--- script.js
+++ script.js
@@ -14,90 +14,33 @@
 // Not equal (!=)
 // Strict not equal (!==)
 
-// 1. Implicit Coercion
-// String to number
-console.log("5" - 2); // 3
-
-// Number to string
-// console.log("5" + 2); // 52
-console.log(`${5 + 2}`); // 7
-
-// Boolean to number
-console.log(true + 1); // 2
-console.log(false + 1); // 1
-
-// Loose equality (==)
-/* console.log("1" == 1); // true
-console.log(true == 1); // true */
-
-// Strict equality (===)
-console.log("1" === 1); // false
-console.log(true === 1); // false
-
-// Boolean Context
-/* if (condition statement) {
-	// body statement
-} else {
-	// body statement
-} */
-
-console.log(Boolean(0));
-// falsy values - false, 0, -0, 0n, "", null, undefined, NaN
-/* if ("str") {
-	console.log("It is true");
-	// true statements here
-} else {
-	console.log("It is false");
-	// false statements here
-} */
-
-// Mixed expression
-// console.log(4 + 2 + true + "7" + 9); // 6 + 1 = 7, "77", "779"
-console.log(6 + 9 - 3 - "5" + 6 - 4 + true); // 15 - 3 = 12, 12 - "5" = 7, 7 + 6 = 13, 13 - 4 = 10
-
-// console.log("6" / "3" + 4 + "5"); // 2 + 4 = 6, "65"
-
+console.log(5 > 2);
+console.log("5" > 2);
+console.log(+null); // 0
 console.log(null == undefined); // true
 console.log(null === undefined); // false
+console.log(null >= 0); // true
 
-/* const a = 0;
-const b = "0";
-console.log(!!a == !!b); // false
-console.log(a == +b); // true */
+console.log("H" > "A"); // true
+console.log("party" > "part"); // true
+console.log("party" > "pert"); // false
+console.log("2" > "14"); // true
+console.log("2".codePointAt(0)); // 50
+console.log("1".codePointAt(0)); // 49
+console.log("hello");
+console.log("hello"[0]); // 'h'
+console.log("H".codePointAt(0)); // 72
+console.log("A".codePointAt(0)); // 65
+console.log("a".codePointAt(0)); // 97
+console.log("a" > "A"); // true
 
-// console.log("hello" / 3); // NaN
+console.log(undefined > 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined < 0); // false
+// console.log(undefined == 0); // false
+console.log(undefined == null); // true
+console.log(undefined === null); // false
 
-console.log(null == 0); // false
-console.log(null === 0); // false
-
-// console.log("5" != 5); // false
-console.log("5" !== 5); // true
-
-console.log(4 + 2 + "8" / "2" - 3 * 2); // 4 + 2 + 4 - 6, 4
-
-// 2. Explicit Coercion (Type-casting)
-// Number(), String(), Boolean(), BigInt()
-const str = "34";
-const toNum = Number(str);
-console.log(toNum, typeof toNum); // 34, number
-
-const text = "This is a text";
-const toNumb = Number(text);
-console.log(toNumb, typeof toNumb); // NaN, number
-
-const numberValue = "";
-const toBool = Boolean(numberValue);
-console.log(toBool, typeof toBool); // true, boolean
-
-const MAX_NUM = 2 ** 53 - 1;
-const MAX_SAFE_NUM = Number.MAX_SAFE_INTEGER;
-console.log(MAX_SAFE_NUM); // 9007199254740991
-console.log(BigInt(MAX_NUM) + 34n); // 9007199254741025n
-console.log(BigInt(MAX_NUM + 34)); // 9007199254741024n
-
-console.log(+null); // 0
-console.log(+undefined); // NaN
-
-const isTrue = 0;
-console.log(!isTrue); // false
-console.log(!!isTrue); // true
+console.log("|".codePointAt(0)); // 124
+console.log("Z".codePointAt(0)); // 90
+console.log("z".codePointAt(0)); // 122
