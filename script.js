66cec2916661eebe2adefceed7f2a7e7b4886f38
Osagie Bello <info@techstackspace.com>
2026-06-03 18:46:53 +0100

    feat: demonstrate ternary statements and nested ternary operations

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // conditional Branching
-// if statement (clause) - if () {...}
+// 1. if statement
+// 2. if...else statement
+// 3. if...else if...else statement
+// 4. ternary statement
+// 5. switch statement
+// 6. nested if statement
+
+// 1. if statement (clause) - if () {...}
 // Syntax
 /* 
 {} - block
@@ -9,12 +16,13 @@ if (condition) {
 }
 */
 
-/* if (undefined) {
+/* 
+if (undefined) {
 	console.log("0 is falsy");
 	console.log("This won't run");
 }
  */
-
+console.log("----------if statement----------");
 const ageJohn = "34";
 const isElder = ageJohn > 20;
 if (isElder) {
@@ -27,8 +35,10 @@ if (ageSarah > 18) {
 	console.log("Sarah has a driving licence");
 }
 
-// else statement (clause) - else {...}
+console.log("----------else statement----------");
+// 2. else statement (clause) - else {...}
 // if...else
+// Syntax
 /* 
 if (condition) {
 	statement(s) executed when the condition is true
@@ -42,8 +52,10 @@ if (ageSarah > 18) {
 	console.log("Sarah is still a teenager");
 }
 
-// else if statement (clause) - else if () {...}
+console.log("----------else if statement----------");
+// 3. else if statement (clause) - else if () {...}
 // else if ...
+// Syntax
 /*
 if (condition1) {
 	statement(s) executed when the condition1 is true
@@ -84,3 +96,95 @@ if (currentYear < 2050) {
 } else {
 	console.log("We are in the Generative AI Era");
 }
+
+// 4. Ternary Statement
+// Ternary Operator (Conditional or question mark operator)
+console.log("----------Ternary statement----------");
+// Syntax
+/* 
+condition ? expression1 : expression2
+*/
+
+const ageMichael = 18;
+const ageBello = 24;
+
+const isYounger =
+	ageMichael < ageBello
+		? "Michael is younger than Bello"
+		: "Bello is younger than Michael";
+
+console.log(isYounger);
+
+// const ageJohn = "34";
+// const isElder = ageJohn > 20;
+if (isElder) {
+	console.log("John is mark's elder brother");
+	console.log("Code will be executed");
+}
+
+// isElder
+// 	? (console.log("John is mark's elder brother"),
+// 		console.log("Code will be executed"))
+// 	: console.log("Condition is false.");
+
+isElder
+	? console.log("John is mark's elder brother")
+	: console.log("Condition is false.");
+
+// const ageSarah = 8;
+// if (ageSarah > 18) {
+// 	console.log("Sarah has a driving license");
+// } else {
+// 	console.log("Sarah is still a teenager");
+// }
+ageSarah > 18
+	? console.log("Sarah has a driving licence")
+	: console.log("Sarah is still a teenager");
+
+// Syntax
+/* 
+condition1 ? expression1 :
+condition2 ? expression2 :
+condition3 ? expression3 :
+...
+expression(n) 
+*/
+// const workHour = 15;
+
+// if (workHour <= 9) {
+// 	console.log("Work hasn't started yet");
+// } else if (workHour >= 12) {
+// 	console.log("Work is most likely started");
+// } else {
+// 	console.log("It is public holiday");
+// }
+
+workHour <= 9
+	? console.log("Work hasn't started yet")
+	: workHour >= 12
+		? console.log("Work is most likely started")
+		: console.log("It is public holiday");
+
+// const currentYear = 2080;
+// if (currentYear < 2050) {
+// 	console.log("We are in the Quantum AI Era");
+// } else if (currentYear === 2080) {
+// 	console.log("We are beyound the Quantum AI Era");
+// } else if (currentYear >= 2080) {
+// 	console.log("We now have robots working for us");
+// } else {
+// 	console.log("We are in the Generative AI Era");
+// }
+
+currentYear < 2050
+	? console.log("We are in the Quantum AI Era")
+	: currentYear === 2080
+		? console.log("We are beyound the Quantum AI Era")
+		: currentYear >= 2080
+			? console.log("We now have robots working for us")
+			: console.log("We are in the Generative AI Era");
+
+const isOlder =
+	ageSarah > 18 ? "Sarah has a driving licence" : "Sarah is still a teenager";
+
+console.log(isOlder);
